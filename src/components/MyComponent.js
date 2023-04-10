f2897ddd3c605a94da6c014dc6a9de2ec8cd699f
hoanglampham <4937910@student.swin.edu.au>
2023-04-10 15:58:03 +1000

    second

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,33 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Hoang Lam Pham',
+        address: 'Swanston st',
+        age: 32
+    };
+
+    handleClick(event) {
+        console.log("Click me my button");
+        console.log("random ", Math.floor((Math.random() * 100) + 1));
+
+        this.setState({
+            name: 'Desmond',
+            age: Math.floor((Math.random() * 100) + 1)
+        });
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event.pageX);
+
+    }
     // JSX
     render() {
         return (
-            <div> My First Component
-                {100 * Math.random()}
+            <div>
+                My name is {this.state.name}, My age is {this.state.age}
+                <button onClick={(event) => this.handleClick()}>Click here</button>
+                <button onMouseOver={this.handleOnMouseOver}>Move over here</button>
             </div>
         );
     }
